39e1122139f5af39801996fa98cb3171f7a59cfe
awsdevopsssp <37306479+awsdevopsssp@users.noreply.github.com>
2022-10-23 12:30:40 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ TASK_NAME=$2
 SERVICE_NAME=$3
 
 
-OLD_IMAGE==$(aws ecs describe-task-definition --task-definition first-run-task-definition | jq '.taskDefinition | .containerDefinitions[0].image' | tr -d '"')
+OLD_IMAGE=$(aws ecs describe-task-definition --task-definition first-run-task-definition | jq '.taskDefinition | .containerDefinitions[0].image' | tr -d '"')
 NEW_IMAGE="435429793199.dkr.ecr.us-east-1.amazonaws.com/mes-rd-test:$BUILD_NUMBER"
 TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition "$TASK_NAME" --region "$REGION")
 NEW_TASK_DEFINITION=$(echo $TASK_DEFINITION | jq --arg IMAGE "$NEW_IMAGE" '.taskDefinition | .containerDefinitions[0].image = $OLD_IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities) | del(.registeredAt) | del(.registeredBy)')
